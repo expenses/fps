be4e43f5b6cbb4938005644ebbd83f60b3da892c
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-01-22 16:55:08 +0100

    Implement a triangle bounding box intersection test. Might not be useful as it doesn't export penetration depth infomation

--- src/intersection_maths.rs
+++ src/intersection_maths.rs
@@ -70,3 +70,99 @@ pub fn ray_triangle_intersection(
         None
     }
 }
+
+// Adapted from https://gdbooks.gitbooks.io/3dcollisions/content/Chapter4/aabb-triangle.html
+fn triangle_bounding_box_intersection(mut a: Vec3, mut b: Vec3, mut c: Vec3, bounding_box: collision_octree::BoundingBox) -> bool {
+    // Convert AABB to center-extents form
+    let center = (bounding_box.min + bounding_box.max) / 2.0;
+    let extents = (bounding_box.max - bounding_box.min) / 2.0;
+
+    // Translate the triangle as conceptually moving the AABB to origin
+    // This is the same as we did with the point in triangle test
+    a -= center;
+    b -= center;
+    c -= center;
+
+    // Compute the edge vectors of the triangle  (ABC)
+    // That is, get the lines between the points as vectors
+    let edge_b_a = b - a;
+    let edge_c_b = c - b;
+    let edge_a_c = a - c;
+
+    // We first test against 9 axis, these axis are given by
+    // cross product combinations of the edges of the triangle
+    // and the edges of the AABB. You need to get an axis testing
+    // each of the 3 sides of the AABB against each of the 3 sides
+    // of the triangle. The result is 9 axis of seperation
+
+    // Compute the 9 axis
+    let axises = [
+        Vec3::unit_x().cross(edge_b_a),
+        Vec3::unit_x().cross(edge_c_b),
+        Vec3::unit_x().cross(edge_a_c),
+
+        Vec3::unit_y().cross(edge_b_a),
+        Vec3::unit_y().cross(edge_c_b),
+        Vec3::unit_y().cross(edge_a_c),
+
+        Vec3::unit_z().cross(edge_b_a),
+        Vec3::unit_z().cross(edge_c_b),
+        Vec3::unit_z().cross(edge_a_c),
+    ];
+
+    for i in 0 .. axises.len() {
+        if seperating_axis_test(a, b, c, axises[i], extents) {
+            return false;
+        }
+    }
+
+    // We can skip this because we already have the bounding box of the triangle.
+    //
+    // Next, we have 3 face normals from the AABB
+    // for these tests we are conceptually checking if the bounding box
+    // of the triangle intersects the bounding box of the AABB
+    // that is to say, the seperating axis for all tests are axis aligned:
+    // axis1: (1, 0, 0), axis2: (0, 1, 0), axis3 (0, 0, 1)
+
+    // Finally, we have one last axis to test, the face normal of the triangle
+    // We can get the normal of the triangle by crossing the first two line segments
+    let triangle_normal = edge_b_a.cross(edge_c_b);
+
+    if seperating_axis_test(a, b, c, triangle_normal, extents) {
+        return false;
+    }
+
+    return true;
+}
+
+fn seperating_axis_test(a: Vec3, b: Vec3, c: Vec3, axis: Vec3, extents: Vec3) -> bool {
+    // Project all 3 vertices of the triangle onto the Seperating axis
+    let a_projected = a.dot(axis);
+    let b_projected = b.dot(axis);
+    let c_projected = c.dot(axis);
+
+    // Project the AABB onto the seperating axis
+    // We don't care about the end points of the prjection
+    // just the length of the half-size of the AABB
+    // That is, we're only casting the extents onto the
+    // seperating axis, not the AABB center. We don't
+    // need to cast the center, because we know that the
+    // aabb is at origin compared to the triangle!
+    let r =
+        extents.x * Vec3::unit_x().dot(axis).abs() +
+        extents.y * Vec3::unit_y().dot(axis).abs() +
+        extents.z * Vec3::unit_z().dot(axis).abs();
+
+    // Now do the actual test, basically see if either of
+    // the most extreme of the triangle points intersects r
+    // You might need to write Min & Max functions that take 3 arguments
+    //
+    // `true` means BOTH of the points of the projected triangle
+    // are outside the projected half-length of the AABB
+    // Therefore the axis is seperating and we can exit
+
+    let max = a_projected.max(b_projected).max(c_projected);
+    let min = a_projected.min(b_projected).min(c_projected);
+
+    (-max).max(min) > r
+}
